3de404758056c28499338fac2c8de793b72ef8b5
Brandon Chan <brandonchan45@gmail.com>
2022-04-04 22:06:31 -0700

    added card template for favorited events to favoirite.html

--- scripts/favorites.js
+++ scripts/favorites.js
@@ -1,6 +1,7 @@
 firebase.auth().onAuthStateChanged(user => {
     if (user) {
         getFavorites(user)
+        getEventFavorites(user)
     } else {
         console.log("No user is signed in");
         window.location.href="login.html"
@@ -45,4 +46,37 @@ function getFavorites(user) {
         })
 }
 
+function getEventFavorites(user){
+    db.collection("users").doc(user.uid).get()
+    .then(userDoc => {
+        var favEvents = userDoc.data().favoriteEvent;
+        
+        let Template = document.getElementById("EventsTemplate");
+        favEvents.forEach(thisEvent =>{
+            db.collection("Events").where("Event", "==", thisEvent).get().then(snap =>{
+                size = snap.size;
+                queryData = snap.docs;
 
+                if (size == 1) {
+                    var doc = queryData[0].data();
+                    var event = doc.Event;
+                    var sport = doc.Sport;
+                    var date = doc.Date;
+                    var type = doc.Type;
+                    var id = doc.ID;
+                    let newcard = Template.content.cloneNode(true);
+                    newcard.querySelector('.event-title').innerHTML = sport;
+                    newcard.querySelector('.event-event').innerHTML = event;
+                    newcard.querySelector('.event-type').innerHTML = type;
+                    newcard.querySelector('.event-date').innerHTML = date;
+                    newcard.querySelector('.event-image').src = "./images/" + id + ".png";
+                    newcard.querySelector('.event-image').onclick = function(){
+                        window.location.href = sport + ".html"
+                    }
+
+                    EventsCardGroup.appendChild(newcard);
+                }
+            })
+        })
+    })      
+}
