9df70bc8b771a56a9248fe33ee30f5b133abb91e
sahilr933 <raisahil580@gmail.com>
2022-04-08 11:56:17 -0700

    Done By Sahil

--- scripts/highlight.js
+++ scripts/highlight.js
@@ -1,11 +1,39 @@
+var currentUser;
+firebase.auth().onAuthStateChanged(user => {
+    if (user) {
+        currentUser = db.collection("users").doc(user.uid);
+        // console.log(currentUser);
+        console.log(currentUser);
+        displayCards();
+    } else {
+        // No user is signed in.
+        console.log("No user is signed in");
+        window.location.href = "loading.html";
+    }
+});
+var user2;
+var i;
+
 function displayCards() {
+
+    currentUser.get()
+    .then(userDoc=>{
+        user2 = userDoc.data().name;
+        
+    })
     let cardTemplate = document.getElementById("AthletesTemplate");
 
     db.collection("users").get()
         .then(snap => {
-            var i = 1;
+            i = 1;
             snap.forEach(doc => {
                     var firstname = doc.data().favoriteath;
+
+                    var user = doc.data().name;
+                   
+                    if (user == user2){
+
+                    
                     // var lastname = doc.data().LastName;
                     // var country = doc.data().Country;
 
@@ -17,11 +45,9 @@ function displayCards() {
                     document.getElementById("Athletes-go-here").appendChild(newcard);
                     i++;
                 
-               
+                    }
             })
         })
-
-
 
 }
 
