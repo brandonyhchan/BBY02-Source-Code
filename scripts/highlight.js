296de2004036e92ef00c6f39ea7fa7452d7bc423
Brandon Chan <brandonchan45@gmail.com>
2022-04-08 12:40:00 -0700

    fixed highlights.js to be fully functioning as intended

--- scripts/highlight.js
+++ scripts/highlight.js
@@ -1,31 +1,42 @@
-function displayCards() {
-    let cardTemplate = document.getElementById("AthletesTemplate");
+firebase.auth().onAuthStateChanged(user => {
+    if (user) {
+        displayHighlights(user)
+    } else {
+        console.log("No user is signed in");
+        window.location.href = "login.html"
+    }
+});
 
-    db.collection("users").get()
-        .then(snap => {
-            var i = 1;
-            snap.forEach(doc => {
-                    var firstname = doc.data().favoriteath;
-                    // var lastname = doc.data().LastName;
-                    // var country = doc.data().Country;
+function displayHighlights(user) {
+    db.collection("users").doc(user.uid).get()
+        .then(userDoc => {
+            var favoritesArray = userDoc.data().favoriteath;
 
-                    let newcard = cardTemplate.content.cloneNode(true);
-                    //update title and text and image
-                    newcard.querySelector('.card-image').src = "./images/" + firstname + "pic1.jpg";                    
-                                        
-                    //attach to gallery
-                    document.getElementById("Athletes-go-here").appendChild(newcard);
-                    i++;
-                
-               
+            let cardTemplate = document.getElementById("AthletesTemplate");
+            favoritesArray.forEach(athlete => {
+                db.collection("pictures").where("name", "==", athlete).get().then(snap => {
+                    size = snap.size;
+                    console.log(size);
+                    queryData = snap.docs;
+
+                    if (size == 1) {
+                        var doc = queryData[0].data();
+                        var name = doc.name;
+                        var picture1 = doc.pic1;
+                        var descr1 = doc.desc1;
+                        let newcard = cardTemplate.content.cloneNode(true);
+                        newcard.querySelector('.card-image').src = "./images/" + name + picture1 + ".jpg";
+                        newcard.querySelector('.card-image').onclick = function(){
+                            window.location.href = name + ".html"
+                        }
+                        newcard.querySelector('.card-desc').innerHTML = descr1;
+                        AthletesCardGroup.appendChild(newcard);
+                    }
+                })
             })
         })
-
-
-
 }
 
-displayCards("users");
 
 
 
